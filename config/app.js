0622fa854b6efe5ec71c8b9bff03a61170590fb1
pengmingzengdd01 <pengmingzeng@hk01.com>
2019-11-28 11:19:20 +0800

    update env

--- config/app.js
+++ config/app.js
@@ -1,7 +1,7 @@
 module.exports = {
-    key: '%$$#$&&**((**&%^*DFSF',
-    name: 'hk01',
+    key: global.CONFIG('APP_KEY','%$$#$&&**((**&%^*DFSF'),
+    name: global.CONFIG('APP_NAME','hk01'),
     port: global.CONFIG('APP_PORT',3000),
-    debug: false,
-    env: 'production',
+    debug: global.CONFIG('DEBUG',false),
+    env: global.CONFIG('ENV','production'),
 }
