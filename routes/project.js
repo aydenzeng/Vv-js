279e8467b1ec7f9559830f6342ec7f3468539d05
pengmingzengdd01 <pengmingzeng@hk01.com>
2019-11-28 11:57:05 +0800

    调整routes目录

--- routes/project.js
+++ routes/project.js
@@ -1,4 +1,4 @@
-const projectCtr = require('../controllers/ProjectController')
+const projectCtr = require('../app/controllers/ProjectController')
 module.exports = async router => {
     router.group("verifyToken2", () => {
         router.get('/project/info', projectCtr.info);
